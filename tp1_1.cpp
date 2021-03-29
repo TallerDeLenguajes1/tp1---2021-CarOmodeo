d585eabeca18bc79a4fd291fbfa59924de0d3da6
CarOmodeo <caro.omodeo@gmail.com>
2021-03-29 11:26:12 -0300

    Punto 1 resuelto

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,5 +2,15 @@
 
 int main(){
 
+    int x = 3;
+    int *px;
+    px = &x;
+
+    printf("Contenido del Puntero: %p \n", px);  
+    printf("Direccion de memoria almacenada : %p \n", px);
+    printf("Direccion de memoria de la variable : %p \n", &x);
+    printf("Direccion de memoria del puntero : %p \n", &px);
+    printf("Tamanio de memoria utilizado por la variable : %d \n", sizeof(x));
+    
     return 0;
 }
